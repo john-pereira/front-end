51508e3d96efd2c048d33fe1654151cf8e8a0c2e
John Pereira <johnev90@gmail.com>
2020-01-19 18:14:11 -0300

    final commit

---
+++ onepage/js/main.js
@@ -0,0 +1,37 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: -19.466499, lng: -42.536961 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    $('#navbar').css("background", "#333");
+  } else {
+    $('#navbar').css("background", "transparent");
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
